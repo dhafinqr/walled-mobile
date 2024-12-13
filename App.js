55bb89852ab35cebbc2700ec70aeb1022844e9d1
Dhafin <dhafinqr@gmail.com>
2024-12-13 10:31:02 +0700

    initial project

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./assets/logo.png")} style={styles.logo} />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        placeholderTextColor="#000000"
+        keyboardType="email-address"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        placeholderTextColor="#000000"
+        secureTextEntry={true}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nomor HP"
+        keyboardType="number-pad"
+        placeholderTextColor="#000000"
+        secureTextEntry={true}
+      />
+
+      {/* TouchableOpacity biar bisa dipencet */}
+      {/* kalo mau sesuatu bisa dipencet bungkus dengan TouchableOpacity */}
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +49,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+  logo: {
+    width: 233,
+    height: 57,
+    marginBottom: 100,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderColor: "#ddd",
+    fontWeight: "bold",
+    borderRadius: 15,
+    paddingHorizontal: 10,
+    marginBottom: 15,
+    backgroundColor: "#f9f9f9",
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "#00A39D",
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 15,
+    width: "100%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
